243684f36857744eda493d00bc4e9e7ec82f587d
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2014-02-19 10:55:11 +0700

    Initial commit, pending/cancel because I can just use perl [-i] -ne'print unless $.==LINENO' FILE

---
+++ lib/App/InsertRemoveLines.pm
@@ -0,0 +1,15 @@
+package App::InsertRemoveLines;
+
+# VERSION
+
+1;
+# ABSTRACT: Insert or remove lines from files/streams
+
+=head1 SYNOPSIS
+
+This distribution provides some command-line utilities:
+
+ insert-lines
+ remove-lines
+
+=cut
